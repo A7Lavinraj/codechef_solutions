5ac1e5dd571d1709d620646d040b3b2b3a970790
LAVIN RAJ MOHAN <107323410+A7Lavinraj@users.noreply.github.com>
2022-06-21 17:02:09 +0530

    Zero Ones Equal One Zeroes
    carefully read the conditions

---
+++ Zero Ones Equal One Zeroes.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vt vector
+#define arr array
+#define pb push_back
+
+#define fore(i, n) for (int i = 0; i < n; ++i)
+#define FOR(i, k, n) for (i = k; k < n ? i < n : i > n ? i += 1 : i -= 1)
+#define PI 3.141592653589793238
+#define SOL 299792458
+
+void solve()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        if(i==0 || i==n-1)
+        cout << '1';
+        else
+        cout << '0';
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t, i = 1;
+    cin>>t;
+    while(t--){
+        //cout<<"#"<<i<<": ";
+        solve();
+        //++i;
+    }
+    return 0;
+}
